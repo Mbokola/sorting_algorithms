9e02a0c9b70aa30008f4e5dba96a6de463f5f7bc
Mbokola <tonypeaks@gmail.com>
2023-06-25 11:38:05 +0000

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, counter = 0, len = size, j;
+	size_t i, counter = 0, len = size;
 
 	while (size > 1)
 	{
@@ -20,13 +20,7 @@ void bubble_sort(int *array, size_t size)
 				array[i] ^= array[i - 1];
 				array[i - 1] ^= array[i];
 				counter = i;
-				for (j = 0; j < len; j++)
-				{
-					if (j > 0)
-						printf(", ");
-					printf("%d", array[j]);
-				}
-				printf("\n");
+				print_array(array, len);
 			}
 		}
 		size = counter;
