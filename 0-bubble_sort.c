5eba9ac15ce05aa5a1d4fe9b9fe4da44de75499c
Mbokola <tonypeaks@gmail.com>
2023-06-25 10:46:07 +0000

    Gitignore file

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - Bubble sort algorithm of an array
+ *@array: Array of integers
+ *@size: Size of array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, counter = 0;
+
+	while (size > 1)
+	{
+		for(i = 1; i < size; i++)
+		{
+			if (array[i] < array[i - 1])
+			{
+				array[i - 1] ^= array[i];
+				array[i] ^= array[i - 1];
+				array[i -1] ^= array[i];
+				counter = i;
+			}
+		}
+		size = counter;
+	}
+}
