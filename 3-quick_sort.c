346eecbc1b6571a68a94a4eb69dbe60ff4e345b6
Mbokola <tonypeaks@gmail.com>
2023-06-27 19:51:06 +0000

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,10 +7,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	quick_sort_helper(array, 0, (int)size - 1, size);
+	quick_sort_helper(array, 0, size - 1, size);
 }
 
 /**
@@ -25,7 +25,6 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 		int pivot = partition(array, low, high, size);
-
 		quick_sort_helper(array, low, pivot - 1, size);
 		quick_sort_helper(array, pivot + 1, high, size);
 	}
@@ -50,7 +49,7 @@ int partition(int *array, int low, int high, size_t size)
 		if (array[j] <= pivot)
 		{
 			i++;
-			if (i != j)
+			if (i != j && array[i] != array[j])
 			{
 				swap(&array[i], &array[j]);
 				print_array(array, size);
@@ -58,8 +57,11 @@ int partition(int *array, int low, int high, size_t size)
 		}
 	}
 
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 
 	return (i + 1);
 }
