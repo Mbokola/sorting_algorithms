39aa91f402f080bc09e882234059fa951d170b6e
Mbokola <tonypeaks@gmail.com>
2024-01-01 17:13:33 +0000

    Garbage collection

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,4 +4,9 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+    """
+    return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
     return ((page * page_size) - page_size, page * page_size)
